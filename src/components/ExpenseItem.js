75e1b04df6dbf376f49864ea68789123fc8e1029
lauretteroy <laurieroy.dev@gmail.com>
2021-06-11 12:01:57 -0700

    Extract css to Card wrapper; Add Expenses, ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,17 @@
 import './ExpenseItem.css';
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-	const month = props.date.toLocaleString('en-US', {month: 'long'});
-	const year = props.date.getFullYear();
-	const day = props.date.toLocaleString('en-US', {day: '2-digit'});
 
   return (
-    <div className="expense-item">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-      <div className="expense-item__description">
+    <Card className='expense-item'>
+			<ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
